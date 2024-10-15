212d95dbbb8602184d41fbe4c64229712aa1baf4
Samarveer15 <Deep.pune@gmail.com>
2024-10-15 19:36:07 +0530

    shutdown

---
+++ Shutdown.py
@@ -0,0 +1,23 @@
+def shutdown(window button):
+    F = input("Is the first step to click the window icon,true/false")
+    if F == true :
+        second_step (power button)
+
+    else:
+        print ("SORRY")
+
+def second_step (power button):
+    G = input("Is the second step to click the power icon,true/false")
+    if G == true :
+        third_step(Shutdown button):
+    
+    else:
+        print("SORRY")
+
+def thir_step (shutdown button):
+    C = input("Is the third step to click the shutdown icon,true/false")
+    if C == true :
+        print("Shutdown") 
+    
+    else:
+        print("Abort shutdown")
